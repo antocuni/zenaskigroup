ae3420d392c2e4fe11262b3de653cd95612211a6
Antonio Cuni <anto.cuni@gmail.com>
2016-12-19 18:43:14 +0100

    update backup script

--- backup.sh
+++ backup.sh
@@ -1,1 +1,3 @@
-scp antocuni.eu:/accounts/antoniocuni/www/zenaskigroup.it/zenaskigroup/project.db backup-`date -I`.db
+#SRC=antocuni.eu:/accounts/antoniocuni/www/zenaskigroup.it/zenaskigroup/project.db
+SRC=uwsgi.it:www/zenaskigroup/project.db
+scp $SRC backup-`date -I`.db
