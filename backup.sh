ac6134ff8194f78d2a4b1efda2ff6ab11114ccf8
Antonio Cuni <anto.cuni@gmail.com>
2016-12-20 00:55:06 +0100

    put the backups into their own directory

--- backup.sh
+++ backup.sh
@@ -1,3 +1,3 @@
 #SRC=antocuni.eu:/accounts/antoniocuni/www/zenaskigroup.it/zenaskigroup/project.db
 SRC=uwsgi.it:www/zenaskigroup/project.db
-scp $SRC backup-`date -I`.db
+scp $SRC backup/project-`date -Im`.db
