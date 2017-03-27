98d6e40928b64808a528a441fa8e419b5ecb006b
Antonio Cuni <anto.cuni@gmail.com>
2017-03-27 14:28:03 +0200

    take also the days left into account

--- trips/testing/test_trips.py
+++ trips/testing/test_trips.py
@@ -1,19 +1,47 @@
+from datetime import date
 from mock import Mock
+from freezegun import freeze_time
 from trips.utils import seats_availability
+
+def availability(trip, left):
+    trip.seats_left = left
+    return seats_availability(trip)
 
 def test_seats_availability():
     trip = Mock(seats=100)
-    trip.seats_left = 100
-    assert seats_availability(trip) == 'alta'
+    trip.date = date(2017, 3, 26)
+    # first tuesday
+    with freeze_time('2017-03-14'):
+        assert availability(trip, left=100) == 'alta'
+        assert availability(trip, left=76) == 'alta'
+        assert availability(trip, left=75) == 'limitata'
+        assert availability(trip, left=50) == 'bassa'
+        assert availability(trip, left=25) == 'critica'
+        assert availability(trip, left=0) == 'esauriti'
+    
+    # first friday, before 12:00: no change
+    with freeze_time('2017-03-17 11:59'):
+        assert availability(trip, left=100) == 'alta'
+        assert availability(trip, left=76) == 'alta'
+        assert availability(trip, left=75) == 'limitata'
+        assert availability(trip, left=50) == 'bassa'
+        assert availability(trip, left=25) == 'critica'
+        assert availability(trip, left=0) == 'esauriti'
+    
+    # first friday, after 12:00: never show "alta"
+    with freeze_time('2017-03-17 12:00'):
+        assert availability(trip, left=100) == 'limitata'
+        assert availability(trip, left=76) == 'limitata'
+        assert availability(trip, left=75) == 'limitata'
+        assert availability(trip, left=50) == 'bassa'
+        assert availability(trip, left=25) == 'critica'
+        assert availability(trip, left=0) == 'esauriti'
     #
-    trip.seats_left = 76
-    assert seats_availability(trip) == 'alta'
-    #
-    trip.seats_left = 75
-    assert seats_availability(trip) == 'limitata'
-    #
-    trip.seats_left = 50
-    assert seats_availability(trip) == 'bassa'
-    #
-    trip.seats_left = 25
-    assert seats_availability(trip) == 'critica'
+    # second tuesday, after 12:00: never show "limitata"
+    with freeze_time('2017-03-21 12:00'):
+        assert availability(trip, left=100) == 'bassa'
+        assert availability(trip, left=76) == 'bassa'
+        assert availability(trip, left=75) == 'bassa'
+        assert availability(trip, left=50) == 'bassa'
+        assert availability(trip, left=25) == 'critica'
+        assert availability(trip, left=0) == 'esauriti'
--- trips/utils.py
+++ trips/utils.py
@@ -1,14 +1,26 @@
+from datetime import datetime
+
 def seats_availability(trip):
+    """
+    Compute an approximate non-numeric estimate of how many seats are left. To
+    convince users to book early, we also take the current time into account:
+    the more we are close to the date, the easier is to display "limitata" or
+    "bassa".
+    """
     left = trip.seats_left
+    dt = trip.date
+    dt = datetime(dt.year, dt.month, dt.day) # convert from "date" to "datetime"
+    delta = (dt - datetime.now())
+    days_left = delta.total_seconds() / (3600*24.0)
+    #
     if left <= 0:
-        # XXX: think about "with reserve" case
         return 'esauriti'
     ratio = float(left) / trip.seats
     if ratio <= 0.25:
         return 'critica'
-    elif ratio <= 0.50:
+    elif ratio <= 0.50 or days_left <= 4.5:
         return 'bassa'
-    elif ratio <= 0.75:
+    elif ratio <= 0.75 or days_left <= 8.5:
         return 'limitata'
     else:
         return 'alta'
