c0da41450969f7fdd7300c30f0d896620dd808bc
Antonio Cuni <anto.cuni@gmail.com>
2017-11-22 11:22:06 +0100

    make sure to display an error message if the user enters an email address which does not exists

--- zenaskigroup/urls.py
+++ zenaskigroup/urls.py
@@ -7,12 +7,21 @@ from django.conf.urls import *  # NOQA
 from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from zenaskigroup.utils import EmailValidationOnForgotPassword
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^jet/', include('jet.urls', 'jet')),  # Django JET URLS
     url(r'', include('trips.urls')),
+
+    url(r'^accounts/password/reset/$',
+        'django.contrib.auth.views.password_reset',
+        { 'post_reset_redirect': '/accounts/password/reset/done/',
+         'html_email_template_name': 'registration/password_reset_email.html',
+         'password_reset_form': EmailValidationOnForgotPassword},
+        name="password_reset"),
+
     url(r'^admin/', include(admin.site.urls)),  # NOQA
     url(r'^sitemap\.xml$', 'django.contrib.sitemaps.views.sitemap',
         {'sitemaps': {'cmspages': CMSSitemap}}),
--- zenaskigroup/utils.py
+++ zenaskigroup/utils.py
@@ -1,3 +1,7 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import PasswordResetForm
+
 class UserTracebackMiddleware(object):
     """
     Adds user to request context during request processing, so that they
@@ -8,3 +12,13 @@ class UserTracebackMiddleware(object):
             request.META['AUTH_USER'] = unicode(request.user.username)
         else:
             request.META['AUTH_USER'] = "Anonymous User"
+
+
+# copied from here, see also urls.py:
+# https://stackoverflow.com/questions/27734185/inform-user-that-email-is-invalid-using-djangos-password-reset
+class EmailValidationOnForgotPassword(PasswordResetForm):
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        if not User.objects.filter(email__iexact=email, is_active=True).exists():
+            raise forms.ValidationError("Non esiste nessun utente con questo indirizzo email")
+        return email
