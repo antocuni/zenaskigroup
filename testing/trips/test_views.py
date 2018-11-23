fe6ab57fced588542d62b5a9cc13cf84debfed9d
Antonio Cuni <anto.cuni@gmail.com>
2018-11-23 12:13:48 +0100

    refactor tests to automatically use fixtures

--- testing/trips/test_views.py
+++ testing/trips/test_views.py
@@ -18,36 +18,54 @@ def trip(db):
                                   content_type='image/jpeg'))
     return t
 
-@pytest.fixture(autouse=True)
+@pytest.fixture
 def testuser(db):
     u = User.objects.create(username='testuser')
     u.set_password('12345')
     u.save()
     return u
 
-def test_next_trip(db, client, trip):
-    assert trip.id == 1
-    resp = client.get('/trip/')
-    assert resp.status_code == 302 # redirect
-    assert resp.url == 'http://testserver/trip/1/'
+class BaseTestView(object):
+
+    @pytest.fixture(autouse=True)
+    def init(self, db, client, testuser, trip):
+        self.trip = trip
+        self.testuser = testuser
+        self.db = db
+        self.client = client
+
+    def login(self):
+        assert self.client.login(username='testuser', password='12345')
+
+    def get(self, *args, **kwargs):
+        return self.client.get(*args, **kwargs)
 
 
-class TestRegister(object):
+class TestNextTrip(BaseTestView):
 
-    def test_login_required(self, db, trip, client):
-        resp = client.get('/trip/1/register/')
+    def test_redirect(self):
+        assert self.trip.id == 1
+        resp = self.get('/trip/')
+        assert resp.status_code == 302 # redirect
+        assert resp.url == 'http://testserver/trip/1/'
+
+
+class TestRegister(BaseTestView):
+
+    def test_login_required(self, trip):
+        resp = self.get('/trip/1/register/')
         assert resp.status_code == 302
         assert resp.url == 'http://testserver/accounts/login/?next=/trip/1/register/'
 
-    def test_registration_allowed(self, db, trip, client):        
+    def test_registration_allowed(self):        
         with freeze_time('2018-12-24 11:59'):
-            client.login(username='testuser', password='12345')
-            resp = client.get('/trip/1/register/')
+            self.login()
+            resp = self.get('/trip/1/register/')
         assert resp.status_code == 200
         assert resp.context['registration_allowed']
 
         with freeze_time('2018-12-24 12:01'):
-            client.login(username='testuser', password='12345')
-            resp = client.get('/trip/1/register/')
+            self.login()
+            resp = self.get('/trip/1/register/')
         assert resp.status_code == 200
         assert not resp.context['registration_allowed']
